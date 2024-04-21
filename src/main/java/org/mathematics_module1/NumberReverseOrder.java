7310ad34402827c3597e89dd9e6e4a8277fa3dd1
sandeepkumar242007 <sandeep_kumar007@outlook.com>
2024-04-21 18:41:58 +0530

    Number reverse order

--- src/main/java/org/mathematics_module1/NumberReverseOrder.java
+++ src/main/java/org/mathematics_module1/NumberReverseOrder.java
@@ -1,4 +1,22 @@
 package org.mathematics_module1;
 
 public class NumberReverseOrder {
+
+    public void revNumber(int num){
+
+        int rev;
+        while (num>0){
+            rev = num%10;
+            System.out.print(rev);
+            num = num/10;
+        }
+    }
+
+    public static void main(String agrs[]){
+
+        int number = 1234;
+        System.out.println("Original Number is : "+number);
+        NumberReverseOrder obj = new NumberReverseOrder();
+        obj.revNumber(number);
+    }
 }
